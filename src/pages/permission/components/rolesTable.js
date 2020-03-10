9abffe7c2a444a467905093f4b8990b990adf3f2
zhousibao <pan.zhou@zhangmen.com>
2020-03-10 16:54:34 +0800

    修改rolesTable组件为Hook

--- src/pages/permission/components/rolesTable.js
+++ src/pages/permission/components/rolesTable.js
@@ -1,104 +1,93 @@
-import React ,{PureComponent} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Table } from 'antd'
 import { rolesList } from '../api'
 import { formatDate } from '@/utils'
 import emitter from '@/utils/eventBus';
 
-export default class  RolesTable extends PureComponent{
-  constructor(props){
-    super(props)
+// const MyComponent = React.memo(function MyComponent(props) {
+//   /* 使用 props 渲染 */
+// });
 
-    this.state ={
-      list:[],
-      loading:false
-    }
-  } 
-  
-  componentDidMount(){
-    this.getRolesList()
+export default function RolesTable({callback}){
+  const [list,setList] = useState([])
+  const [loading,setLoading] = useState(false)
 
-    this.eventEmitter = emitter.addListener('reloadRolesTable', () => {
-      this.getRolesList()
+  useEffect(() => {
+    getRolesList()
+    const eventEmitter = emitter.addListener('reloadRolesTable', () => {
+      getRolesList()
     });
 
-  }
-  componentWillUnmount() {
-    emitter.removeListener(this.eventEmitter);
-  }
+    return () => {
+      emitter.removeListener(eventEmitter);
+    } 
+  },[])
 
   
-  getRolesList  = () => {
-    this.setState({
-      loading:true
-    })
+  const getRolesList = () => {
+    setLoading(true)
 
     rolesList().then(res => {
       if(res.code === '0'){
-        this.setState({
-          list:res.data.list,
-          loading:false
-        })
+        setList(res.data.list)
+        setLoading(false)
       }
     }).catch(err => {
-      this.setState({
-        loading:false
-      })
+      setLoading(false)
     })
-    
   }
+
+  const rowSelection = {
+    type: 'radio',
+    onSelect: (item) => {
+      callback(item.id)
+    }
+  };
+
+  const columns = [
+    {
+      title: '角色ID',
+      dataIndex: 'id'
+    }, 
+    {
+      title: '角色名称',
+      dataIndex: 'role_name'
+    },
+    {
+      title: '创建时间',
+      dataIndex: 'create_time',
+      render: create_time => formatDate(create_time)
+    }, 
+    {
+      title: '使用状态',
+      dataIndex: 'status',
+      render: status => status === 1 ? "启用":"关闭" 
+    }, 
+    {
+      title: '授权时间',
+      dataIndex: 'authorize_time',
+      render: authorize_time => formatDate(authorize_time)
+    }, 
+    {
+      title: '授权人',
+      dataIndex: 'authorize_user_name'
+    }
+  ];
+
     
-  render(){
-    const rowSelection = {
-      type: 'radio',
-      onSelect: (item) => {
-        this.props.callback(item.id)
-      }
-    };
-
-
-    const columns = [
-      {
-        title: '角色ID',
-        dataIndex: 'id'
-      }, 
-      {
-        title: '角色名称',
-        dataIndex: 'role_name'
-      },
-      {
-        title: '创建时间',
-        dataIndex: 'create_time',
-        render: create_time => formatDate(create_time)
-      }, 
-      {
-        title: '使用状态',
-        dataIndex: 'status',
-        render: status => status === 1 ? "启用":"关闭" 
-      }, 
-      {
-        title: '授权时间',
-        dataIndex: 'authorize_time',
-        render: authorize_time => formatDate(authorize_time)
-      }, 
-      {
-        title: '授权人',
-        dataIndex: 'authorize_user_name'
-      }
-    ];
-    return (
-      <Table
-        loading={this.state.loading}
-        bordered
-        scroll={{
-          x:true,
-          y:400
-        }}
-        columns={columns}
-        dataSource={this.state.list}
-        rowSelection={rowSelection}
-        pagination={false}
-      />
-    )
-  }
+  return (
+    <Table
+      loading={loading}
+      bordered
+      scroll={{
+        x:true,
+        y:400
+      }}
+      columns={columns}
+      dataSource={list}
+      rowSelection={rowSelection}
+      pagination={false}
+    />
+  )
   
 }
