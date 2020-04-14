1956e82d4da3e51a885c949be7efd0fdb33012c5
zhousibao <pan.zhou@zhangmen.com>
2020-04-14 21:32:01 +0800

    添加event

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,10 +8,21 @@ export default class Home extends Component {
       systemName: 'react-admin',
     }
   }
+  handleC = (event) => {
+    console.log(event)
+    console.log(event.target)
+    console.log(event.currentTarget)
+
+    console.log(event.nativeEvent)
+    console.log(event.nativeEvent.target)
+    console.log(event.nativeEvent.currentTarget)
+
+
+  }
   render() {
     return (
       <div className="home">
-        <p className="title">欢迎使用{this.state.systemName}管理系统</p>
+        <p className="title" onClick={(e) => this.handleC(e)}>欢迎使用{this.state.systemName}管理系统</p>
         <img src="https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3" alt="" className="img"/>
       </div>
     )
