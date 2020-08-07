e54b640b10938d53775f4c3bc9731b0d4668bdb1
zhousibao <pan.zhou@zhangmen.com>
2020-08-07 20:50:41 +0800

    add css

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/index.css';
+import '@/style/index.less';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from '@/redux'
